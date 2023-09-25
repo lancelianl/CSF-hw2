184030cb1c4ebe46b62520ae3469b8d5f19a9004
lancelianl <112418548+lancelianl@users.noreply.github.com>
2023-09-24 20:59:14 -0400

    c functions and main

--- c_wcfuncs.c
+++ c_wcfuncs.c
@@ -21,7 +21,11 @@
 // Note that the character values should be treated as
 // being unsigned (in the range 0..255)
 uint32_t wc_hash(const unsigned char *w) {
-  // TODO: implement
+  uint32_t hash_code = 5381;
+  for (; *w; w++) {
+    hash_code = hash_code * 33 + (uint32_t)*w; 
+  }
+  return hash_code;
 }
 
 // Compare two strings lexicographically. Return
@@ -35,12 +39,22 @@ uint32_t wc_hash(const unsigned char *w) {
 // of the other, it is considered as "less than". E.g.,
 // "hi" would compare as less than "high".
 int wc_str_compare(const unsigned char *lhs, const unsigned char *rhs) {
-  // TODO: implement
+  while (*lhs && (*lhs == *rhs)) {
+        lhs++;
+        rhs++;
+    }
+    return *(const unsigned char *)lhs - *(const unsigned char *)rhs;
 }
 
 // Copy NUL-terminated source string to the destination buffer.
 void wc_str_copy(unsigned char *dest, const unsigned char *source) {
   // TODO: implement
+  while (*source) {
+        *dest = *source;
+        dest++;
+        source++;
+    }
+    *dest = '\0'; 
 }
 
 // Return 1 if the character code in c is a whitespace character,
@@ -56,12 +70,24 @@ void wc_str_copy(unsigned char *dest, const unsigned char *source) {
 //   '\v'
 int wc_isspace(unsigned char c) {
   // TODO: implement
+  switch (c) {
+        case ' ':
+        case '\t':
+        case '\r':
+        case '\n':
+        case '\f':
+        case '\v':
+            return 1;
+        default:
+            return 0;
+    }
 }
 
 // Return 1 if the character code in c is an alphabetic character
 // ('A' through 'Z' or 'a' through 'z'), 0 otherwise.
 int wc_isalpha(unsigned char c) {
   // TODO: implement
+  return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
 }
 
 // Read the next word from given input stream, storing
@@ -77,18 +103,50 @@ int wc_isalpha(unsigned char c) {
 // characters in the sequence should be stored in the array.
 int wc_readnext(FILE *in, unsigned char *w) {
   // TODO: implement
+    unsigned char *word = w;
+    int c;
+
+    do {
+        c = fgetc(in);
+        if (c == EOF) {
+            return 0;  
+        }
+    } while (wc_isspace(c));
+
+    // Read the word
+    while (!wc_isspace((unsigned char)c) && word - w < MAX_WORDLEN) {
+        *word++ = (unsigned char)c;
+        c = fgetc(in);
+        if (c == EOF) {
+          *word = '\0';
+          return 1;
+          break;  // End of file
+        }
+    }
+    *word = '\0';  
+    while (!wc_isspace((unsigned char)c) && c != EOF) c = fgetc(in);
+
+    return 1;
 }
 
 // Convert the NUL-terminated character string in the array
 // pointed-to by w so that every letter is lower-case.
 void wc_tolower(unsigned char *w) {
   // TODO: implement
+  while (*w != '\0') {
+    *w = (*w >= 'A' && *w <= 'Z') ? (*w - 'A' + 'a') : *w;
+    w++;
+  }
+
 }
 
 // Remove any non-alphaabetic characters from the end of the
 // NUL-terminated character string pointed-to by w.
 void wc_trim_non_alpha(unsigned char *w) {
   // TODO: implement
+  while (*w != '\0') w++;
+  while (!wc_isalpha(*w)) w--; 
+  *++w = '\0';
 }
 
 // Search the specified linked list of WordEntry objects for an object
@@ -105,6 +163,22 @@ void wc_trim_non_alpha(unsigned char *w) {
 // job to update the count.)
 struct WordEntry *wc_find_or_insert(struct WordEntry *head, const unsigned char *s, int *inserted) {
   // TODO: implement
+  // if(head) printf("append");
+  struct WordEntry *curr = head;
+  while (curr){
+    unsigned char *curr_str = (unsigned char *)curr->word;
+    if (wc_str_compare(curr_str, s) == 0) {
+            *inserted = 0;
+            return curr;
+      }
+  curr = curr->next;
+  } 
+  struct WordEntry *newEntry = (struct WordEntry *)malloc(sizeof(struct WordEntry));
+  wc_str_copy((unsigned char *)newEntry->word, s);
+  newEntry->count = 0;
+  newEntry->next = head;
+  *inserted = 1;
+  return newEntry;
 }
 
 // Find or insert the WordEntry object for the given string (s), returning
@@ -116,9 +190,23 @@ struct WordEntry *wc_find_or_insert(struct WordEntry *head, const unsigned char 
 // which represents s.
 struct WordEntry *wc_dict_find_or_insert(struct WordEntry *buckets[], unsigned num_buckets, const unsigned char *s) {
   // TODO: implement
+  int inserted = -1;
+  uint32_t hashIndex = wc_hash(s) % num_buckets;
+  struct WordEntry *newEntry = wc_find_or_insert(buckets[hashIndex], s, &inserted);
+  if (inserted) {
+    buckets[hashIndex] = newEntry;
+  }
+  return newEntry;
 }
 
 // Free all of the nodes in given linked list of WordEntry objects.
 void wc_free_chain(struct WordEntry *p) {
   // TODO: implement
+  struct WordEntry *curr = p;
+  struct WordEntry *next;
+  while (curr) {
+    next = curr->next;
+    free(curr);
+    curr = next;
+  }
 }
--- c_wcmain.c
+++ c_wcmain.c
@@ -5,6 +5,7 @@
 // Suggested number of buckets for the hash table
 #define HASHTABLE_SIZE 13249
 
+
 // TODO: prototypes for helper functions
 
 int main(int argc, char **argv) {
@@ -16,13 +17,53 @@ int main(int argc, char **argv) {
 
   // TODO: implement
 
-  printf("Total words read: %u\n", (unsigned int) total_words);
-  printf("Unique words read: %u\n", (unsigned int) unique_words);
-  printf("Most frequent word: %s (%u)\n", (const char *) best_word, best_word_count);
+//   while ( next word is read successfully using wc_readnext ) {
+//   increase total word count by 1
+
+//   use wc_tolower to convert word to lower case
+
+//   use wc_trim_non_alpha to remove non-alphabetic characters at end of word
+
+//   use wc_dict_find_or_insert to find or insert the word in the hash table
+
+//   increment the WordEntry's count
+// }
+
+  FILE *file = fopen(argv[1], "r");
+  struct WordEntry *hashTable[HASHTABLE_SIZE] = {NULL};
+  unsigned char word[MAX_WORDLEN + 1];
+  while (wc_readnext(file, word)) {
+    total_words++;
+    wc_tolower(word);
+    wc_trim_non_alpha(word);
+    wc_dict_find_or_insert(hashTable, HASHTABLE_SIZE, word) -> count++;
+  }
+  
+  struct WordEntry *curr;
+  for (int i = 0; i < HASHTABLE_SIZE; i++){
+    if (hashTable[i]){
+      curr = hashTable[i]; 
+      while (curr) {
+        unique_words++; 
+        if (curr->count > best_word_count) {
+          best_word_count = curr->count;
+          best_word = curr->word;
+        }
+        curr = curr->next;
+      }
+    } 
+  }
+  
+  printf("Total words read: %u\n", total_words);
+  printf("Unique words read: %u\n",  unique_words);
+  printf("Most frequent word: %s (%u)\n",  best_word, best_word_count);
 
   // TODO: make sure file is closed (if one was opened)
   // TODO: make sure memory is freed
-
+  fclose(file);
+  for (int i = 0; i < HASHTABLE_SIZE; i++){
+    if (hashTable[i]) wc_free_chain(hashTable[i]);
+  }
   return 0;
 }
 
